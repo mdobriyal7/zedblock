1f585e4956df80980cb87c3264f86a59fd26d3be
Manish <mdobriyal7@gmail.com>
2023-05-30 09:44:04 +0530

    minor change

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
-// const allowedOrigins = [
-//     'https://technotes.onrender.com'
-// ]
+const allowedOrigins = [
+    'https://zedblock-server.onrender.com'
+]
 
-// module.exports = allowedOrigins
+module.exports = allowedOrigins
