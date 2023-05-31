c11700df70da5c962c98aa8e22c8e9e7a754a0f9
Manish <mdobriyal7@gmail.com>
2023-05-31 14:21:39 +0530

    cors commit

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,15 +1,18 @@
-const allowedOrigins = require('./allowedOrigins')
-
-const corsOptions = {
+const allowedOrigins = [
+    'https://your-react-app-domain.com'
+  ];
+  
+  const corsOptions = {
     origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
     },
     credentials: true,
     optionsSuccessStatus: 200
-}
-
-module.exports = corsOptions 
+  };
+  
+  module.exports = corsOptions;
+  
