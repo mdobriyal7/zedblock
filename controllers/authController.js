c82298a65281e565794a742ecd985217b1c83939
Manish <mdobriyal7@gmail.com>
2023-05-31 16:30:08 +0530

    time

--- controllers/authController.js
+++ controllers/authController.js
@@ -30,13 +30,13 @@ const login = async (req, res) => {
             }
         },
         process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn: '15m' }
+        { expiresIn: '15s' }
     )
 
     const refreshToken = jwt.sign(
         { "username": foundUser.username },
         process.env.REFRESH_TOKEN_SECRET,
-        { expiresIn: '7d' }
+        { expiresIn: '2m' }
     )
 
     // Create secure cookie with refresh token 
